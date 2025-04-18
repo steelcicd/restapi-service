cb5171eb847f17eaf83fac826e50f83d043560a1
Anish Hirlekar <anishsaas@gmail.com>
2025-04-19 00:51:22 +0530

    Added missing $ variable notation

--- push-image.sh
+++ push-image.sh
@@ -5,9 +5,10 @@ echo 'Building image...'
 # build image
 docker build -t $REGISTRY_URI:latest .
 
-echo 'Pushing image...'
 # login to ecr
-aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin REGISTRY_URI
+echo 'Logging in to ECR...'
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $REGISTRY_URI
 # push image
+echo 'Pushing image...'
 docker push REGISTRY_URI:latest
 echo 'Image pushed successfully!'
